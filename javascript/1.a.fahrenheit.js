2a351a51e47d1c47dee49f43984109a67ef9e7b6
C.B <aschera@gmail.com>
2016-10-19 09:22:57 +0200

    Update 1.a.fahrenheit.js

--- javascript/1.a.fahrenheit.js
+++ javascript/1.a.fahrenheit.js
@@ -1,4 +1,12 @@
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Fahrenheit/Celcius</title>
+
+
 <script type="text/javascript">
 
 /* program to convert Celcius temperatures to Fahrenheit*/
@@ -8,6 +16,16 @@ var number = prompt("What is the temperature in Celcius?", "");
 var celcius = number;
 var fahrenheit = number * 9/5 + 32;
 
-console.log(fahrenheit);
+//console.log(fahrenheit);
+document.write(fahrenheit);
 
 </script>
+
+
+</head>
+    
+<body>
+
+                                        
+</body>
+</html>

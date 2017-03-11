a19d4acc82a32d9eeaab0547063bff3a17335002
aschera <aschera@gmail.com>
2017-03-11 08:16:59 +0100

    changed the delete function

--- books/delete_item.js
+++ books/delete_item.js
@@ -1,15 +1,72 @@
-var something = '';
+
 
 window.addEventListener('load', function() { // so shit loads and then runs.
-
+    
+    
 let deletion = document.getElementById('delete');  
     
-deletion.addEventListener('click', function() {    
+deletion.addEventListener('click', function() {   
+    
+    let res = document.querySelector('input[name="views"]:checked').value;
 
-	document.getElementById(something).outerHTML = "";
-    console.log('deleting ' + something);
-    //need to call api and delete entry on database also!
+    let element = document.getElementById(res).innerText; // the li that is selected
+    var number = parseFloat(element.match(/-*[0-9]+/));  // the id number in that li
+
+	
+    
+    
+    
+    //////////////////   AJAX ///////////////////////////////////////
+    
+    // url to access server
+            let url = 'https://www.forverkliga.se/JavaScript/api/crud.php?op=delete&key=sHx2P';
+    
+            url+= 'id=' + number;
+       console.log(url);
+            // AJAX request 
+            let ajax2 = new XMLHttpRequest();
+
+            ajax2.open('post', url);
+
+            ajax2.onreadystatechange = function() {
+
+
+                if (ajax2.status == 200 && ajax2.readyState == 4) {
+                    //parse string to object
+                    let json2 = JSON.parse(ajax2.responseText);
+
+                    // check is status = error
+                    if (json2.status !== 'error') {
+
+                        // status message - Server OK
+                        printm(json2.status + ' : ' + json2.message);
+                        
+                        document.getElementById(res).outerHTML = ""; // delete from view list
+
+                    } else {
+                        console.log('server is no good');
+                        console.log(json2);
+
+                        // status message - Server BAD
+                        printm(json2.status + ' : ' + json2.message);
+                        //myFunction();
+                    }
+
+                } else if (ajax2.status != 200) {
+                    // AJAX failiure report
+                    console.log('ajax error');
+                }
+
+            }; //end ajax
+            ajax2.send();
+
  
 });
     
 });
+
+
+
+
+
+
--- books/view_book.js
+++ books/view_book.js
@@ -3,84 +3,86 @@ var something = '';
 
 
 
-        // View book data
-        window.addEventListener('load', function() { // so shit loads and then runs.
-            let button_view = document.getElementById('button2');
-            
-            button_view.addEventListener('click', function() {
+// View book data
+window.addEventListener('load', function() { // so shit loads and then runs.
+    let button_view = document.getElementById('button2');
 
-                // url to access server
-                let url = 'https://www.forverkliga.se/JavaScript/api/crud.php?op=select&key=sHx2P';
+    button_view.addEventListener('click', function() {
 
-                // AJAX request 
-                let ajax2 = new XMLHttpRequest();
+            // url to access server
+            let url = 'https://www.forverkliga.se/JavaScript/api/crud.php?op=select&key=sHx2P';
 
-                ajax2.open('get', url);
-  
-                ajax2.onreadystatechange = function() {
-   
+            // AJAX request 
+            let ajax2 = new XMLHttpRequest();
 
-                    if (ajax2.status == 200 && ajax2.readyState == 4) {
-                        //parse string to object
-                        let json2 = JSON.parse(ajax2.responseText);
+            ajax2.open('get', url);
 
-                        // check is status = error
-                        if (json2.status !== 'error') {
-                            
-                            console.log('server is all good!');
-                            
-    // status message - Server OK
-                             printm('You are now viewing a list of all the books stored.');
- 
-                            for (i=0; i < json2.data.length; i++) {
-                                
+            ajax2.onreadystatechange = function() {
+
+
+                if (ajax2.status == 200 && ajax2.readyState == 4) {
+                    //parse string to object
+                    let json2 = JSON.parse(ajax2.responseText);
+
+                    // check is status = error
+                    if (json2.status !== 'error') {
+
+                        console.log('server is all good!');
+
+                        // status message - Server OK
+                        printm('You are now viewing a list of all the books stored.');
+
+                        for (i = 0; i < json2.data.length; i++) {
+
                             var node = document.createElement("LI");
                             node.className += " list-group-item";
-                            node.id += 'ckeck' + i;;
-                                
-                                var checkbox = document.createElement("INPUT");
-                                checkbox.setAttribute("type", "radio");
-                                checkbox.value = node.id;
-                                checkbox.name = 'views';
-                                 
+                            node.id += 'check' + i;;
+
+                            var checkbox = document.createElement("INPUT");
+                            checkbox.setAttribute("type", "radio");
+                            checkbox.onclick = "handleClick()";
+                            checkbox.value = node.id;
+                            checkbox.name = 'views';
+
                             node.appendChild(checkbox);
                             node.style.backgroundColor = '#9f4636';
-                            node.style.color = '#f1dcc9';    
-                                
-                            var textnode = document.createTextNode(' id: ' + json2.data[i].id + ', title:  ' + json2.data[i].title  + ', Author:  ' + json2.data[i].author  + ', updated:  ' + json2.data[i].updated + '.');         
-                            node.appendChild(textnode); 
-                              
+                            node.style.color = '#f1dcc9';
+
+                            var textnode = document.createTextNode(' id: ' + json2.data[i].id + ', title:  ' + json2.data[i].title + ', Author:  ' + json2.data[i].author + ', updated:  ' + json2.data[i].updated + '.');
+                            node.appendChild(textnode);
+
                             document.getElementById("books").appendChild(node);
                             
-                            something = node.id;
-                            
-                            }
-                            
-                            console.log(ajax2.responseText);
-                            
-
-                        } else {
-                            console.log('server is no good');
-                            console.log(json2);
-                            
-        // status message - Server BAD
-                            printm(json2.message);
-                            //myFunction();
+                            //  save this items id in the global variable.
+                            //something = node.id;
                         }
 
-                    } else if (ajax2.status != 200) {
-                        // AJAX failiure report
-                        console.log('ajax error');
+                        console.log(ajax2.responseText);
+
+
+                    } else {
+                        console.log('server is no good');
+                        console.log(json2);
+
+                        // status message - Server BAD
+                        printm(json2.status + ' : ' + json2.message);
+                        //myFunction();
                     }
 
-                };  //end ajax
+                } else if (ajax2.status != 200) {
+                    // AJAX failiure report
+                    console.log('ajax error');
+                }
+
+            }; //end ajax
             ajax2.send();
-                
-                
-//delete element
-function deleteListitem() {  
-	document.getElementById(something).outerHTML = "";
-};
-            } // button event listener done
-        
-        )}); // window event listener done
+
+
+            //delete element
+            function deleteListitem() {
+                document.getElementById(something).outerHTML = "";
+            };
+        } // button event listener done
+
+    )
+}); // window event listener done

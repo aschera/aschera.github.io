5b841cede8f1c4ff7b71242caf30a1fae4f5aeb2
aschera <aschera@gmail.com>
2017-03-24 09:33:13 +0100

    trying to fix local storage

---
+++ chatapp/login.js
@@ -0,0 +1,97 @@
+ window.addEventListener('load', function() { // so shit loads and then runs.
+     
+        
+        // Read value from storage, or empty array
+        names = JSON.parse(localStorage.getItem('locname') || "[]");
+
+        console.log(names);
+     
+       
+        // load the values into a select menu
+        loadSelectList();
+     
+ });
+
+/* ************************ Log in via SELECT **************************** */
+
+        function logIn2() {
+		let n = document.getElementById("saved_names").selectedIndex;
+        let t = document.getElementsByTagName("option")[n].value;
+        
+		if(n >= 1) {
+			
+			   let loginwindow = document.getElementById('logInWindow');
+			    loginwindow.style.display = 'none';
+			    
+			    console.log('yes');
+			    let results = document.getElementById('results');
+			    results.style.display = 'block';
+
+			    let inputText = document.getElementById('text');
+			    inputText.style.display = 'inline-block';
+			    
+			    let postButton = document.getElementById('post');
+			    postButton.style.display = 'inline-block';
+            
+                let writeText = document.getElementById('textline');
+			    writeText.style.display = 'inline-block';
+                
+                let seeText = document.getElementById('chat_text');
+			    seeText.style.display = 'inline-block';
+			    
+			    let loginInfo = document.getElementById('logininfo');
+			    loginInfo.style.display = 'inline-block';
+		
+			    let logoutbutton = document.getElementById('logout');
+			    logoutbutton.style.display = 'inline-block';
+            
+                let n = document.getElementById("saved_names").selectedIndex;
+                let t = document.getElementsByTagName("option")[n].value;
+                document.getElementById('loginname').innerText = t;
+		}
+		else {
+			console.log('please enter a username');
+			window.alert('please enter a username');
+		}
+
+         
+};
+   /* ************************ Log in via TEXTFIELD **************************** */
+
+        function logIn() {
+		let n = document.querySelector('#username').value;
+		if(n.length > 1) {
+			console.log(document.querySelector('#username'));
+			   let loginwindow = document.getElementById('logInWindow');
+			    loginwindow.style.display = 'none';
+			    
+			    console.log('yes');
+			    let results = document.getElementById('results');
+			    results.style.display = 'block';
+
+			    let inputText = document.getElementById('text');
+			    inputText.style.display = 'inline-block';
+			    
+			    let postButton = document.getElementById('post');
+			    postButton.style.display = 'inline-block';
+            
+                let writeText = document.getElementById('textline');
+			    writeText.style.display = 'inline-block';
+                
+                let seeText = document.getElementById('chat_text');
+			    seeText.style.display = 'inline-block';
+			    
+			    let loginInfo = document.getElementById('logininfo');
+			    loginInfo.style.display = 'inline-block';
+		
+			    let logoutbutton = document.getElementById('logout');
+			    logoutbutton.style.display = 'inline-block';
+			    
+			    document.getElementById('loginname').innerText = usernameInput.value;
+		}
+		else {
+			console.log('please enter a username');
+			window.alert('please enter a username');
+		}
+        
+};
---
+++ chatapp/logout.js
@@ -0,0 +1,53 @@
+    
+ /* ************************ log out **************************** */
+	 function forgetData() {
+
+        let name = document.getElementById('loginname').innerText;
+	    //localStorage.removeItem(name);  //does not work!
+         
+        //localStorage.setItem(name, "");
+        localStorage.removeItem(name);
+        
+        loadSelectList();
+		location.reload();
+        
+        
+        };
+
+
+/* ************************ ADD items to dropdown list **************************** */
+    
+function loadSelectList() {
+    if (names.length > 1) {
+        for (i = 0; i < names.length; i++) {
+            let item = document.getElementById('saved_names');
+            let option = document.createElement("option");
+                option.text = names[i].name;
+                option.id = names[i].name;
+                option.value = names[i].name;
+                item.add(option);
+        }
+    }
+}; 
+
+
+ /* ************************ ADD username to localstorage **************************** */
+        // Read value from storage, or empty array
+        names = JSON.parse(localStorage.getItem('locname') || "[]");
+
+        function saveData() {
+
+            var data = document.getElementById('username').value;
+
+            //unique ID for the user
+            let id = data.length + 'id' + Math.floor((Math.random() * 100) + 1) + data;
+		
+            names.push({
+                id: id,
+                name: data
+            });
+
+            localStorage.setItem('locname', JSON.stringify(names));
+
+            console.log(names);
+        };
